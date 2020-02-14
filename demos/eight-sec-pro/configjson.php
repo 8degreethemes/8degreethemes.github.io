69acb3250db7bc933ffda855dd287d799cf2c6ea
8degreethemes <8degreethemes@gmail.com>
2020-02-14 14:43:50 +0545

    Added: eight sec pro demo

---
+++ demos/eight-sec-pro/configjson.php
@@ -0,0 +1,162 @@
+<?php 
+/**
+* Demo import configuration file
+*
+*
+*/
+
+$git_url 		= 'https://raw.githubusercontent.com/8degreethemes/8degreethemes.github.io/master/demos/eight-sec-pro/';
+$data = array(
+	'default' => array(
+		'demo_name' => 'default',
+		'xml_file'     		=> $git_url . 'default/content.xml',
+		'widgets_file'  	=> $git_url . 'default/widgets.wie',
+		'theme_settings'  	=> $git_url . 'default/customizer_options.dat',
+		'screen'			=> $git_url . 'default/default.png',
+		'preview_url'		=> 'https://8degreethemes.com/demo/eight-sec/default',
+		'is_shop'			=> true,
+		'home_title'  		=> 'Home',
+		'blog_title'  		=> '',
+		'menus' 			=> array(
+			'primary' => 'Menu 1',
+		),
+		'required_plugins'  => array(
+			'free' => array(
+				array(
+					'slug'      => 'ultimate-form-builder-lite',
+					'init' 	=> 'ultimate-form-builder-lite/ultimate-form-builder-lite.php',
+					'name' 	=> 'Ultimate Form Builder Lite',
+				),
+				array(
+					'slug'  	=> 'woocommerce',
+					'init'  	=> 'woocommerce/woocommerce.php',
+					'name'  	=> 'WooCommerce',
+				)
+			),
+			'premium' => array(
+				array(
+					'slug' 		=> 'revslider',
+					'init'  	=> 'revslider/revslider.php',
+					'name' 		=> 'Slider Revolution',
+					'class'		=> 'RevSlider',
+				)
+			)
+		)
+	),
+	'travel' => array(
+		'demo_name' => 'travel',
+		'xml_file'     		=> $git_url . 'travel/content.xml',
+		'widgets_file'  	=> $git_url . 'travel/widgets.wie',
+		'theme_settings'  	=> $git_url . 'travel/customizer_options.dat',
+		'screen'			=> $git_url . 'travel/travel.png',
+		'preview_url'		=> 'https://8degreethemes.com/demo/eight-sec/travel',
+		'is_shop'			=> true,
+		'home_title'  		=> 'Home',
+		'blog_title'  		=> '',
+		'menus' 			=> array(
+			'primary' => 'Menu 1',
+		),
+		'required_plugins'  => array(
+			'free' => array(
+				array(
+					'slug'      => 'ultimate-form-builder-lite',
+					'init' 	=> 'ultimate-form-builder-lite/ultimate-form-builder-lite.php',
+					'name' 	=> 'Ultimate Form Builder Lite',
+				),
+				array(
+					'slug'  	=> 'woocommerce',
+					'init'  	=> 'woocommerce/woocommerce.php',
+					'name'  	=> 'WooCommerce',
+				)
+			),
+			'premium' => array(
+				array(
+					'slug' 		=> 'revslider',
+					'init'  	=> 'revslider/revslider.php',
+					'name' 		=> 'Slider Revolution',
+					'class'		=> 'RevSlider',
+				)
+			)
+		)
+	),
+	'app' => array(
+		'demo_name' => 'app',
+		'xml_file'     		=> $git_url . 'app/content.xml',
+		'widgets_file'  	=> $git_url . 'app/widgets.wie',
+		'theme_settings'  	=> $git_url . 'app/customizer_options.dat',
+		'screen'			=> $git_url . 'app/app.png',
+		'preview_url'		=> 'https://8degreethemes.com/demo/eight-sec/app',
+		'is_shop'			=> true,
+		'home_title'  		=> 'Home',
+		'blog_title'  		=> '',
+		'menus' 			=> array(
+			'primary' => 'Menu 1',
+		),
+		'required_plugins'  => array(
+			'free' => array(
+				array(
+					'slug'      => 'ultimate-form-builder-lite',
+					'init' 	=> 'ultimate-form-builder-lite/ultimate-form-builder-lite.php',
+					'name' 	=> 'Ultimate Form Builder Lite',
+				),
+				array(
+					'slug'  	=> 'woocommerce',
+					'init'  	=> 'woocommerce/woocommerce.php',
+					'name'  	=> 'WooCommerce',
+				)
+			),
+			'premium' => array(
+				array(
+					'slug' 		=> 'revslider',
+					'init'  	=> 'revslider/revslider.php',
+					'name' 		=> 'Slider Revolution',
+					'class'		=> 'RevSlider',
+				)
+			)
+		)
+	),
+	'grad' => array(
+		'demo_name' => 'grad',
+		'xml_file'     		=> $git_url . 'grad/content.xml',
+		'widgets_file'  	=> $git_url . 'grad/widgets.wie',
+		'theme_settings'  	=> $git_url . 'grad/customizer_options.dat',
+		'screen'			=> $git_url . 'grad/grad.png',
+		'preview_url'		=> 'https://8degreethemes.com/demo/eight-sec/grad',
+		'is_shop'			=> true,
+		'home_title'  		=> 'Home',
+		'blog_title'  		=> '',
+		'menus' 			=> array(
+			'primary' => 'Menu 1',
+		),
+		'required_plugins'  => array(
+			'free' => array(
+				array(
+					'slug'      => 'ultimate-form-builder-lite',
+					'init' 	=> 'ultimate-form-builder-lite/ultimate-form-builder-lite.php',
+					'name' 	=> 'Ultimate Form Builder Lite',
+				),
+				array(
+					'slug'  	=> 'woocommerce',
+					'init'  	=> 'woocommerce/woocommerce.php',
+					'name'  	=> 'WooCommerce',
+				)
+			),
+			'premium' => array(
+				array(
+					'slug' 		=> 'revslider',
+					'init'  	=> 'revslider/revslider.php',
+					'name' 		=> 'Slider Revolution',
+					'class'		=> 'RevSlider',
+				)
+			)
+		)
+	),
+);
+	//return $data;
+echo (json_encode($data));
+
+$fp = fopen('config.json', 'w');
+fwrite($fp, json_encode($data));
+fclose($fp);
+
+echo "<h2>Check the folder (".getcwd().")  and find config.json downloaded there.</h2>";
